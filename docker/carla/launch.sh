c3e58206ebbfab849efd2c811e0e139d299b41b4
Sanjay Shankar <155586731+SanjayS598@users.noreply.github.com>
2026-01-27 17:24:15 -0500

    Starting project skeleton

---
+++ docker/carla/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# CARLA server launch script with low-fidelity settings
+
+# TODO: Implement in Phase 1
+# Will include flags for:
+# - Offscreen rendering
+# - Low quality preset
+# - Fixed FPS mode
